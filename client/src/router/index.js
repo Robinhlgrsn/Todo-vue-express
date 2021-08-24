03bfaf93c4666b9ca2106db7c7356141864cc844
robinhlgrsn <robinholgersson91@gmail.com>
2021-08-24 14:35:40 +0200

    added Addtodo Route

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
+import AddTodo from '@/views/AddTodo.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: Home,
+  },
+  {
+    path: '/addtodo',
+    name: 'AddTodo',
+    component: AddTodo,
   },
 ];
 
---
+++ client/src/views/AddTodo.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Add New todo!</h1>
+</template>
+
+<script>
+export default {
+  name: 'AddTodo',
+};
+</script>
+
+<style>
+
+</style>
